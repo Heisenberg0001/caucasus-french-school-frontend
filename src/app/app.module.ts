75a805fca5b96dcad01a3716b61ff6894d4cc75b
AnaMaghradze <annmaghradze@gmail.com>
2022-11-28 00:35:45 +0400

    feat(translations): add TranslateService, fonts, update styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppSharedModule } from './shared/shared.module';
-import {FormsModule} from "@angular/forms";
+import { AppSharedModule } from '@shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, AppSharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    AppSharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/services/language.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, Renderer2 } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+const LANGUAGE = 'LANGUAGE';
+const DEFAULT_LANGUAGE = 'en';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LanguageService {
+  languages: Language[] = [];
+  private _currentLanguage!: Language;
+
+  constructor(private translateService: TranslateService, private renderer: Renderer2) {
+    this.languages = LANGUAGES;
+  }
+
+  getPreferredLanguage(): Language {
+    let preferredLanguage;
+    const preferredLangJSON = localStorage.getItem(LANGUAGE);
+    if (preferredLangJSON) {
+      preferredLanguage = JSON.parse(preferredLangJSON);
+    } else {
+      preferredLanguage = LANGUAGES[0];
+      localStorage.setItem(LANGUAGE, JSON.stringify(preferredLanguage));
+    }
+    return preferredLanguage;
+  }
+
+  initialize(): void {
+    this._currentLanguage = this.getPreferredLanguage();
+    this.translateService.addLangs(this.languages.map((x) => x.key));
+    this.translateService.setDefaultLang(DEFAULT_LANGUAGE);
+    this.use(this._currentLanguage);
+  }
+
+  use(language: Language): void {
+    this._currentLanguage = language;
+    localStorage.setItem(LANGUAGE, JSON.stringify(this._currentLanguage));
+    this.translateService.use(language?.key);
+    this.changeGlobalStylesByLanguage(this._currentLanguage?.key);
+  }
+
+  private changeGlobalStylesByLanguage(languageKey: string): void {
+    /* use different font for different language */
+    if (languageKey === 'ka') {
+      this.renderer.removeClass(document.body, 'font-adieu');
+      this.renderer.addClass(document.body, 'font-noto-sans-georgian');
+    } else {
+      this.renderer.removeClass(document.body, 'font-noto-sans-georgian');
+      this.renderer.addClass(document.body, 'font-adieu');
+    }
+  }
+}
+
+export const LANGUAGES: Language[] = [
+  { name: 'ENG', key: 'en', locale: 'en-us' },
+  { name: 'GEO', key: 'ka', locale: 'ka-ge' },
+  { name: 'FR', key: 'fr', locale: 'fr-fr' },
+];
+
+export interface Language {
+  name: string;
+  key: string;
+  locale: string;
+}
