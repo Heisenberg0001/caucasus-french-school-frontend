3706525be6865864466c2da509417afccaee4271
AnaMaghradze <annmaghradze@gmail.com>
2022-12-01 00:57:41 +0400

    feat(pages): create skeleton for home & other pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { AppSharedModule } from '@shared/shared.module';
+import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {HomeComponent} from "./pages";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -28,7 +29,9 @@ export function HttpLoaderFactory(http: HttpClient) {
       },
     }),
     AppSharedModule,
+    HomeComponent,
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent],
 })
